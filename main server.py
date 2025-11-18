77107fb48ba90accbd85b869e2aa65c87e591185
E-coal-eater <lefeuvre.eric8566@gmail.com>
2025-11-18 11:29:00 +0100

    Version 0.3 (the pilot update Part 1)
    removed pipotam.gif
    added a new pilot-stats page with a different password and username

--- main server.py
+++ main server.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-VALID_USERNAME = 'les-déraillés'
-VALID_PASSWORD = 'Paulette'
+VALID_USERNAME1 = 'Prog_derailles'
+VALID_PASSWORD1 = 'siperprogrammeur'
+VALID_USERNAME2 = 'pilot_derailles'
+VALID_PASSWORD2 = 'siperpilote'
 
 @app.route('/')
 def enter():
@@ -14,15 +16,21 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
-        if username == VALID_USERNAME and password == VALID_PASSWORD:
-            return redirect(url_for('control', username=username))
+        if username == VALID_USERNAME1 and password == VALID_PASSWORD1:
+            return redirect(url_for('control'))
+        elif username == VALID_USERNAME2 and password == VALID_PASSWORD2:
+            return redirect(url_for('pilot'))
         else:
             return redirect(url_for('login?err=invalidLogin'))
     return render_template('login.html')
 
 @app.route('/control')
-def home():
+def control():
     return render_template('control.html')
-    
+
+@app.route('/pilot')
+def pilot():
+    return render_template('pilot-stats.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
