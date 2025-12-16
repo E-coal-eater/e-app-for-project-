0b706076e9497e451b67e6619cd799eba5912319
E-coal-eater <lefeuvre.eric8566@gmail.com>
2025-12-16 15:38:12 +0100

    Version 0.4.0 (the data base update)
    added the databases velos and parcours on the main python script
    added small web page change on the control.html to the new page db_test.hmtl
    added db_test.hmtl used to test render the databases then it will be prob put on the main control one

--- main server.py
+++ main server.py
@@ -1,10 +1,40 @@
 from flask import Flask, render_template, request, redirect, url_for
+import os
+import sqlite3
 
 app = Flask(__name__)
 VALID_USERNAME1 = 'Prog_derailles'
 VALID_PASSWORD1 = 'siperprogrammeur'
 VALID_USERNAME2 = 'pilot_derailles'
 VALID_PASSWORD2 = 'siperpilote'
+
+def init_db():
+    # si le fichier de base de données n'existe pas : on crée la base de données
+    if not os.path.exists(DB_NAME):
+        conn = sqlite3.connect(DB_NAME)
+        cursor = conn.cursor()
+        cursor.execute('''
+        CREATE TABLE velos(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            modele TEXT,
+            parcours_id INTEGER NOT NULL,
+            FOREIGN KEY (parcours_id) REFERENCES parcours(id)
+        );
+        ''')
+        cursor.execute('''
+        CREATE TABLE parcours (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id_velo INTEGER NOT NULL,
+            FOREIGN KEY (id_velo) REFERENCES velos(id),
+            temps INT,
+            vitesse INT,
+            location TEXT,
+            battery INT
+        );
+        ''')
+        conn.commit
+        conn.close
+init_db
 
 @app.route('/')
 def enter():
@@ -32,6 +62,24 @@ def control():
 @app.route('/pilot')
 def pilot():
     return render_template('pilot-stats.html')
+
+@app.route('/db_test')
+def db():
+    conn = sqlite3.connect(DB_NAME)
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor
+
+    #render des 2 databases pour la page HTML
+    cursor.execute("SELECT * FROM velos")
+    velos = cursor.fetchall()
+
+    cursor.execute("SELECT * FROM parcours")
+    parcours = cursor.fetchall()
+
+    conn.close()
+    
+    return render_template('db_test.hmtl', velos=velos, parcours=parcours)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
