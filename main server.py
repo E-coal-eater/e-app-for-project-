0533e98bc2bc7643e0a20ba33f7edeb0f7ddf95f
E-coal-eater <lefeuvre.eric8566@gmail.com>
2026-01-13 15:32:27 +0100

    Version 0.6.1 (Serial update part 2)
    imported threading
    
    change little stuff on the db initiation
    separation into different parts to find each part more fish
    addeda whole arduino part (not functionnal and useless because new change of plan with electron stuff added
    added approute livepoints wich refresh the databases in real time while the whole server is running
    added a refresh cript in DB.HTML

--- main server.py
+++ main server.py
@@ -2,19 +2,13 @@ from flask import Flask, render_template, request, redirect, url_for
 import os
 import sqlite3
 import serial
+import threading
+
 
 app = Flask(__name__)
-TESTUser = 'A'
-TESTPassword = 'A'
-TESTUser2 = 'B'
-TESTPassword2 = 'B'
-VALID_USERNAME1 = 'Prog_derailles'
-VALID_PASSWORD1 = 'siperprogrammeur'
-VALID_USERNAME2 = 'pilot_derailles'
-VALID_PASSWORD2 = 'siperpilote'
-
 DB_NAME = 'user.db'
 
+#---------------- Database initialisation ---------------------------
 def init_db():
     # si le fichier de base de données n'existe pas : on crée la base de données
     if not os.path.exists(DB_NAME):
@@ -31,14 +25,15 @@ def init_db():
         CREATE TABLE parcours (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             id_velo INT,
-            temps INT NOT NULL,
+            temps INT,
             FOREIGN KEY (id_velo) REFERENCES velos(id)
        )
        ''')
         cursor.execute('''
         CREATE TABLE points(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
             id_parcours INT NOT NULL,
-            temps INT NOT NULL,
+            temps INT,
             battery INT,
             FOREIGN KEY (id_parcours) REFERENCES parcours(id)
         )
@@ -47,6 +42,86 @@ def init_db():
         conn.close()
 init_db()
 
+#------------------------ Arduino Part -----------------------------------
+
+SERIAL_PORT = 'COM31'
+BAUDRATE = 9600
+
+reccording = False
+current_parcours_id = None
+last_temps = 0
+
+def serial_listener():
+    global reccording, current_parcours_id, last_temps
+
+    try:
+        ser = serial.Serial(SERIAL_PORT, BAUDRATE, timeout=1)
+    except:
+        print("Serial not connected")
+        return
+    
+    conn = sqlite3.connect(DB_NAME, check_same_thread=False)
+    cursor = conn.cursor()
+    cursor.execute("PRAGMA foreign_keys ON")
+
+    while True:
+        line = ser.readline().decode().strip()
+        if not line:
+            continue
+        
+        if line == 'BTN':
+
+            if not reccording:
+                reccording = True
+
+                cursor.execute("SELECT id FROM velos ORDER BY id DESC LIMIT 1")
+                row = cursor.fetchone()
+                if not row:
+                    print("no velo in DB")
+                    reccording = False
+                    continue
+                
+                last_velo_id = row[0]
+
+                cursor.execute("INSERT INTO parcours(id_velo, temps) VALUE(?, NULL)", (last_velo_id))
+                current_parcours_id = cursor.lastrowid
+                conn.commit()
+
+                print("Started Parcours", current_parcours_id)
+            else:
+                reccording = False
+
+                cursor.execute("""
+                UPDATE parcours
+                SET temps = ?
+                WHERE id = ?
+                """, (last_temps, current_parcours_id))
+                
+                conn.commit()
+                print("Parcours stopped", current_parcours_id, "Final Time:", last_temps)
+                current_parcours_id = None
+        elif line.startswith("DATA") and reccording:
+            _, temps, battery = line.split(",")
+
+            last_temps = int(temps)
+
+            cursor.execute("""
+            INSERT INTO points (id_parcours, temps, battery)
+            VALUES (?,?,?)
+            """, (current_parcours_id, temps, battery))
+            conn.commit()
+
+threading.Thread(target=serial_listener, daemon=True) .start()
+
+#------------------------Web Part ----------------------------------------
+TESTUser = 'A'
+TESTPassword = 'A'
+TESTUser2 = 'B'
+TESTPassword2 = 'B'
+VALID_USERNAME1 = 'Prog_derailles'
+VALID_PASSWORD1 = 'siperprogrammeur'
+VALID_USERNAME2 = 'pilot_derailles'
+VALID_PASSWORD2 = 'siperpilote'
 @app.route('/')
 def enter():
     return render_template('main-page.html')
@@ -80,7 +155,7 @@ def DB():
     conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
 
-    #render des 2 databases pour la page HTML
+    #render des 3 databases pour la page HTML
     cursor.execute("SELECT * FROM velos")
     velos = cursor.fetchall()
 
@@ -89,7 +164,7 @@ def DB():
 
     cursor.execute("SELECT * FROM points")
     points = cursor.fetchall()
- 
+
     conn.close()
 
     if request.method == 'POST':
@@ -107,9 +182,22 @@ def DB():
             conn.commit()
             conn.close()
         return redirect(url_for('DB'))
-
     err = request.args.get('err')
     return render_template('DB.html', velos=velos, parcours=parcours, err=err)
 
+@app.route('livePoints')
+def livePoints():
+    conn = sqlite3.connect(DB_NAME)
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    SELECT points.id, points.id_parcours, points.temps, points.battery
+    FROM points
+    ORDER BY points.id ASC
+    """)
+    data = [dict(row) for row in cursor.fetchall()]
+    conn.close()
+    return data
 if __name__ == '__main__':
     app.run(debug=True)
